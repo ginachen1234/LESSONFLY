a1017e90d22668e0397241ec8f7a108c09acc31e
Julio Quintana <julioeq29@gmail.com>
2019-02-26 11:09:03 +0000

    set up associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   validates :student_id, presence: true
   validates :teacher_id, presence: true
-  belongs_to :user
-  belongs_to :teacher, class_name: "User"
+  belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
+  belongs_to :student, class_name: "User", foreign_key: "student_id"
 end
