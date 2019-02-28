9467d21d9c4c1a3447b7ae7ee43128c4327affb8
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-02-28 23:26:00 +0000

    user show page fixed & map zoom speed changed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, speed: 5 });
 };
 
 const initMapbox = () => {
