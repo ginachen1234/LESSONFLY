cb8a754de0d1209d6ec766460f37c7452ec58d77
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-02-28 10:57:57 +0000

    user authentification changed to be displayes after the user choose a lesson, now a user can see the lessons availables witout log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @teachers = User.where.not(latitude: nil, longitude: nil).where(teacher: true)
 
