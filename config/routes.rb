d966e4bdf4ff9cefa63e2d1b93569b4245e7727d
Julio Quintana <julioeq29@gmail.com>
2019-02-28 13:44:25 +0000

    a signed-in user can become a teacher

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
-  resources :users, only:[ :index, :show ] do
+  resources :users, only:[ :index, :show, :edit, :update ] do
     resources :bookings
   end
 end
